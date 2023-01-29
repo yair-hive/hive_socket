177248bb2bec076832f02c2822b3b775d4c217ad
yair man <u0504171788@gmail.com>
2023-01-29 15:42:02 +0200

    sv

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const { WebSocketServer } = require("ws");
+process.env.FORCE_COLOR=true
+
+import chalk from 'chalk';
+import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 3001 });
 
+wss.on('listening', ()=> console.log(chalk.green('web socket start')))
+
 wss.on('connection', function connection(ws) {
+    console.log(`
+    connection starts`)
     ws.on('message', function message(data) {
         wss.clients.forEach(function(client) {
             client.send(data.toString());
         });        
     });
 });
+
+wss.on('error', (err)=> console.log(err))
